4718a30e168832633b14a7872893f869ab5053a2
vladimirovicp <vladimirovicp70@gmail.com>
2022-09-24 23:08:27 +0400

    feat: add 3 секции

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,35 @@ icons.forEach (icon => {
         icon.classList.toggle("open");
     });
 });
+
+
+
+// Слайдер
+const swiperZoo = new Swiper('.zoo__slider', {
+    // direction: 'vertical',
+    // loop: true,
+    // spaceBetween: 30,
+
+    slidesPerView: 3,
+    grid: { rows: 2, },
+
+    // breakpoints: {
+    //     320: {
+    //         slidesPerView: 2
+    //     },
+    //     768: {
+    //         slidesPerView: 3
+    //     }
+    // },
+
+
+    // pagination: {
+    //     el: '.hero__pagination',
+    // },
+
+    navigation: {
+        nextEl: '.zoo__arrow-next',
+        prevEl: '.zoo__arrow-prev',
+    },
+
+});
